3218e1a640597e4ac7f2892f324934e3a5e7b4c7
ThuanTayNguyen <yasuomonmon1205@gmail.com>
2017-10-17 15:23:26 +0700

    Complete

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 
 /*
 Hello world 
@@ -7,10 +6,4 @@ Hello world
         require_once('controllers/post_controller.php');
 		$getPost= new PostController();
 		$getPost->getPost();
-
-=======
-  require_once('controllers/post_controller.php');
-  $getPost= new PostController();
-  $getPost->getPost();
->>>>>>> 0fda0443f5ef6b8c6a03cc94687b85ef7363e39e
 ?>
