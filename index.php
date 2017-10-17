0fda0443f5ef6b8c6a03cc94687b85ef7363e39e
youkiery <youkiery@gmail.com>
2017-10-17 14:59:56 +0700

    Tui

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-
-			require_once('controllers/post_controller.php');
-			$getPost= new PostController();
-			$getPost->getPost();
-
+  require_once('controllers/post_controller.php');
+  $getPost= new PostController();
+  $getPost->getPost();
 ?>
